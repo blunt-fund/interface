4e25bbf7a7c99e493523071df8cd4d966191a5e2
Jacopo <39241410+jjranalli@users.noreply.github.com>
2022-07-31 16:13:44 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+const colors = require("tailwindcss/colors")
+
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  purge: [
+    "./pages/**/*.tsx",
+    "./pages/**/*.js",
+    "./components/**/*.tsx",
+    "./components/**/*.js",
+    "./components/**/*.css",
+    "./components/**/*.scss",
+    "./styles/**/*.scss",
+    "./utils/**/*.ts",
+    "./lib/**/*.ts",
+    "./lib/**/*.tsx",
+    "./styles/jit-hack.txt"
+  ],
+
+  theme: {
+    // nightwind: {
+    //   colorClasses: ["gradient", "placeholder"],
+    //   typography: {
+    //     strong: {
+    //       color: colors.white
+    //     }
+    //   },
+    //   colors: {
+    //     white: "#0F1115",
+    //     black: "gray.50",
+    //     sky: {
+    //       50: "#001928"
+    //     }
+    //   }
+    // },
+    extend: {
+      screens: {
+        xs: "560px"
+      },
+      colors: {
+        gray: colors.gray,
+        sky: colors.sky,
+        cyan: colors.cyan
+      },
+      zIndex: {
+        "-10": "-10"
+      }
+    }
+  },
+  variants: {
+    // nightwind: ["group-hover"],
+  },
+
+  plugins: [
+    require("@tailwindcss/typography")
+    // require("nightwind")
+  ]
+}
