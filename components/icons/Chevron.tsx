fb1722aff3ceba5ea8768e6ae9c1777c0dfd484c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-08-03 23:32:38 +0200

    icons

--- components/icons/Chevron.tsx
+++ components/icons/Chevron.tsx
@@ -1,17 +1,22 @@
 const Chevron = ({ ...props }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-chevron-left w-full h-full"
-      {...props}
-    >
-      <polyline points="15 18 9 12 15 6"></polyline>
+    <svg viewBox="0 0 38 38" fill="none" className="w-full h-full" {...props}>
+      <rect
+        x="19"
+        y="0.615234"
+        width="26"
+        height="5"
+        transform="rotate(45 19 0.615234)"
+        fill="currentColor"
+      />
+      <rect
+        x="33.8491"
+        y="15.4648"
+        width="5"
+        height="26"
+        transform="rotate(45 33.8491 15.4648)"
+        fill="currentColor"
+      />
     </svg>
   )
 }
