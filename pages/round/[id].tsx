723bfd65ee0794fc9721ad076f39d139a90e44e6
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-08-06 22:50:58 +0200

    created round/[id] page

---
+++ pages/round/[id].tsx
@@ -0,0 +1,55 @@
+import { NextSeo } from "next-seo"
+import { Container, RoundViewMain } from "@components/ui"
+import {
+  defaultDescription,
+  defaultTitle,
+  longTitle,
+  domain
+} from "@components/common/Head"
+import { useRouter } from "next/router"
+
+export default function Create() {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <>
+      <NextSeo
+        title={`Blunt round #${id} | Blunt Finance`}
+        openGraph={{
+          title: longTitle,
+          description: defaultDescription,
+          url: domain,
+          images: [
+            {
+              url: `${domain}/og_image.jpg`,
+              width: 1000,
+              height: 1000,
+              alt: `${defaultTitle} cover image`
+            }
+          ]
+        }}
+      />
+      <Container page={true}>
+        <main className="max-w-screen-sm mx-auto space-y-10 ">
+          <h1>Blunt round #{id}</h1>
+          <RoundViewMain
+            name={"test"}
+            descriptionHtml={""}
+            image={{ url: "", file: undefined }}
+            website={""}
+            twitter={"@jj_ranalli"}
+            discord={""}
+            docs={""}
+            tokenSymbol={"TEST"}
+            tokenIssuance={1000}
+            duration={2}
+            target={3}
+            cap={4}
+            isFundraiseEth={true}
+          />
+        </main>
+      </Container>
+    </>
+  )
+}
