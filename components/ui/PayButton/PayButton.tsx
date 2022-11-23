ec2e58f3bf5e768341dbdeaa0723b3ebb87e9992
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-23 03:22:29 +0100

    updated PayButton

--- components/ui/PayButton/PayButton.tsx
+++ components/ui/PayButton/PayButton.tsx
@@ -2,12 +2,18 @@ import { Input } from "@components/ui"
 import { useAddRecentTransaction } from "@rainbow-me/rainbowkit"
 import executeTransaction from "@utils/executeTransaction"
 import formatNumber from "@utils/formatNumber"
-import { ethers } from "ethers"
+import { addresses } from "@utils/constants"
+import { BigNumber, ethers } from "ethers"
 import { Dispatch, SetStateAction, useState } from "react"
 import { useContractWrite, usePrepareContractWrite } from "wagmi"
+import JBTerminal from "abi/JBETHPaymentTerminal.json"
+import { RoundData } from "../CreateRoundForm/CreateRoundForm"
+import { useAppContext } from "../context"
+import useNormalizeCurrency from "@utils/useNormalizeCurrency"
 
 type Props = {
-  round: any
+  projectId: number
+  round: RoundData
   payment: number
   isSlicerToBeCreated: boolean
   isPaymentEth: boolean
@@ -16,6 +22,7 @@ type Props = {
 }
 
 const PayButton = ({
+  projectId,
   round,
   payment,
   isSlicerToBeCreated,
@@ -23,15 +30,31 @@ const PayButton = ({
   setPayment,
   setIsPaymentEth
 }: Props) => {
+  const { account } = useAppContext()
   const [loading, setLoading] = useState(false)
   const addRecentTransaction = useAddRecentTransaction()
+  const paymentEth = useNormalizeCurrency(payment, isPaymentEth)
+
   const { config } = usePrepareContractWrite({
-    addressOrName: "JBTerminal",
-    contractInterface: "",
+    address: addresses.JBTerminal,
+    abi: JBTerminal.abi,
     functionName: "pay",
-    args: [],
+    args: [
+      projectId,
+      0,
+      ethers.constants.AddressZero,
+      account,
+      0,
+      false,
+      "Paid from BF",
+      []
+    ],
     overrides: {
-      value: payment // TODO: FIX
+      value:
+        payment != 0 &&
+        BigNumber.from(10)
+          .pow(15)
+          .mul(Math.floor(paymentEth * 1000)) // TODO: Write better?
     }
   })
 
@@ -61,11 +84,13 @@ const PayButton = ({
         Receive{" "}
         <span className="font-bold text-blue-600">
           {isSlicerToBeCreated &&
-            `1k slices ${round.tokenIssuance != 0 ? "+ " : ""}`}
+            `${
+              payment ? formatNumber(Math.floor(paymentEth * 1000)) : "1k"
+            } slices ${round.tokenIssuance != 0 ? "+ " : ""}`}
           {round.tokenIssuance != 0 &&
             `${formatNumber(
               payment
-                ? Number(Number(payment * round.tokenIssuance).toFixed(0))
+                ? Number(Number(paymentEth * round.tokenIssuance).toFixed(0))
                 : round.tokenIssuance,
               3
             )} 
