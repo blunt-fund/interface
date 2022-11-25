00875899177467be470068195b0775bc51e24723
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-25 21:55:55 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   swcMinify: true,
+  images: {
+    domains: [
+      // process.env.NEXT_PUBLIC_APP_URL.split("://").pop().split(":")[0],
+      // process.env.NEXT_PUBLIC_SUPABASE_URL.split("://").pop(),
+      "ipfs.io"
+      // "static.alchemyapi.io",
+      // ""
+    ]
+  }
 }
