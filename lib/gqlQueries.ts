e457ac58c0b96d997e74eca124cdb047c8d34f9e
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-27 18:07:01 +0100

    added tokensQueryProject

--- lib/gqlQueries.ts
+++ lib/gqlQueries.ts
@@ -1,5 +1,23 @@
 import { addresses } from "utils/constants"
 
+export const tokensQueryProject = (
+  projectId: number | string
+) => /* GraphQL */ `
+project(
+  id: "2-${projectId}"
+) {
+  owner
+  createdAt
+  metadataUri
+  configureEvents {
+    timestamp
+    duration
+    weight
+    dataSource
+  }
+}
+`
+
 export const tokensQueryProjects = /* GraphQL */ `
 projects(
   where: {
