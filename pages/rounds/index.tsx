3ec18ab6efe84a9537996d23e40f6c6ac6a28fd0
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-30 23:42:31 +0100

    fix

--- pages/rounds/index.tsx
+++ pages/rounds/index.tsx
@@ -32,7 +32,7 @@ export default function Explore({ subgraphData, projectData }) {
       <Container page={true}>
         <main className="max-w-screen-sm mx-auto">
           <h1 className="pb-20">Blunt rounds</h1>
-          {/* <RoundsList subgraphData={subgraphData} projectData={projectData} /> */}
+          <RoundsList subgraphData={subgraphData} projectData={projectData} />
         </main>
       </Container>
     </>
