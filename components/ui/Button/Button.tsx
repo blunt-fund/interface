90cb08c50c568fc5ad86862ef50b57486fe2f3c8
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-30 17:26:45 +0100

    updated button styling

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -11,6 +11,7 @@ interface ButtonProps {
   href?: string
   onClick?: any
   secondary?: boolean
+  disabled?: boolean
 }
 
 const Button: FC<ButtonProps> = (props) => {
@@ -23,6 +24,7 @@ const Button: FC<ButtonProps> = (props) => {
     onClick,
     loading = false,
     secondary = false,
+    disabled = false,
     ...rest
   } = props
 
@@ -30,10 +32,11 @@ const Button: FC<ButtonProps> = (props) => {
     customClassName || secondary
       ? "px-5 overflow-hidden text-sm font-bold tracking-wide rounded-sm"
       : "overflow-hidden font-bold tracking-wide rounded-sm"
-  const color =
-    customColor || secondary
+  const color = !disabled
+    ? customColor || secondary
       ? "dark:!text-white text-blue-600 border-blue-600 border-2 hover:bg-blue-700 focus:bg-blue-700 hover:text-white"
       : "text-white bg-blue-600 hover:bg-blue-700 focus:bg-blue-700"
+    : "bg-gray-400 dark:!bg-gray-500 cursor-not-allowed"
 
   const rootClassName = `nightwind-prevent px-7 h-[40px] min-w-[150px] focus:outline-none ${color} ${className}`
 
@@ -42,7 +45,12 @@ const Button: FC<ButtonProps> = (props) => {
       <button className={rootClassName}>{label}</button>
     </Link>
   ) : (
-    <button className={rootClassName} type={type} onClick={onClick}>
+    <button
+      className={rootClassName}
+      type={type}
+      disabled={disabled}
+      onClick={() => !disabled && !loading && onClick}
+    >
       <div className="flex items-center justify-center w-full">
         {loading ? <Spinner /> : <p>{label}</p>}
       </div>
