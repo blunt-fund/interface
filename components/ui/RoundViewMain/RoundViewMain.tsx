12938bf222dbddf0fec15abe8589767a4f6f94a9
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-11-30 20:05:06 +0100

    added juicebox link in RoundViewMain

--- components/ui/RoundViewMain/RoundViewMain.tsx
+++ components/ui/RoundViewMain/RoundViewMain.tsx
@@ -1,7 +1,7 @@
 import File from "@components/icons/File"
 import Link from "@components/icons/Link"
 import Logo from "@components/icons/Logo"
-import { Discord, Twitter } from "@components/icons/Social"
+import { Discord, Juicebox, Twitter } from "@components/icons/Social"
 import Image from "next/image"
 import { ConditionalLink, RoundDetails } from "@components/ui"
 import { RoundData } from "utils/getRounds"
@@ -64,50 +64,58 @@ const RoundViewMain = ({
               {name}
             </h1>
             {!roundId ? (
-              (website || twitter || discord || docs) && (
-                <div className="flex items-center gap-6 mt-4 ml-1">
-                  {website && (
-                    <a
-                      className="w-5 h-5 higlight"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={website}
-                    >
-                      <Link />
-                    </a>
-                  )}
-                  {twitter && (
-                    <a
-                      className="w-5 h-5 higlight"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={twitterUrl}
-                    >
-                      <Twitter />
-                    </a>
-                  )}
-                  {discord && (
-                    <a
-                      className="w-5 h-5 mt-0.5 higlight"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={discord}
-                    >
-                      <Discord />
-                    </a>
-                  )}
-                  {docs && (
-                    <a
-                      className="w-5 h-5 higlight"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={docs}
-                    >
-                      <File />
-                    </a>
-                  )}
-                </div>
-              )
+              <div className="flex items-center gap-6 mt-4 ml-1">
+                {website && (
+                  <a
+                    className="w-5 h-5 higlight"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={website}
+                  >
+                    <Link />
+                  </a>
+                )}
+                {twitter && (
+                  <a
+                    className="w-5 h-5 higlight"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={twitterUrl}
+                  >
+                    <Twitter />
+                  </a>
+                )}
+                {discord && (
+                  <a
+                    className="w-5 h-5 mt-0.5 hover:text-indigo-500"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={discord}
+                  >
+                    <Discord />
+                  </a>
+                )}
+                {docs && (
+                  <a
+                    className="w-5 h-5 hover:text-gray-500"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={docs}
+                  >
+                    <File />
+                  </a>
+                )}
+                <a
+                  className="w-5 h-5 hover:text-yellow-600"
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`https://${
+                    process.env.NEXT_PUBLIC_CHAIN_ID == "5" ? "goerli." : ""
+                  }juicebox.money/v2/p/${roundId}`}
+                >
+                  <Juicebox />
+                </a>
+              </div>
             ) : (
               <p className="pt-2 text-sm">
                 Round allocation: <b>{shares[0]}%</b>
