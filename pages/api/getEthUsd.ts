92c64ced9d1c2f4cf5e41d88338dcd17eb6895c1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-06 14:38:12 +0100

    temp

--- pages/api/getEthUsd.ts
+++ pages/api/getEthUsd.ts
@@ -4,27 +4,15 @@ import fetcher from "@utils/fetcher"
 import { ethers } from "ethers"
 
 const getQuotes = async (req: NextApiRequest, res: NextApiResponse) => {
-  await corsMiddleware(req, res)
+  // await corsMiddleware(req, res)
   try {
     if (req.method === "GET") {
-      // const cmcUrl = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1027&convert=USD`
-      // const cmcKey = process.env.COIN_MARKET_CAP_KEY
-      // const headers = {
-      //   "X-CMC_PRO_API_KEY": cmcKey,
-      //   Accept: "application/json"
-      // }
-
-      // const { data } = await fetcher(cmcUrl, {
-      //   method: "GET",
-      //   headers: headers
-      // })
-
-      // const quote = data[1027].quote.USD.price
-
-      const { price } = await fetcher(
+      const data = await fetcher(
         "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
       )
-      res.status(200).json(price)
+      console.log(data)
+
+      res.status(200).json(data?.price)
     }
   } catch (err) {
     console.log(err)
