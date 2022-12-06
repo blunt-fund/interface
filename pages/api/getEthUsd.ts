5e9aca09457d9a8b9f6fed8eaa188007a0e7816c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-06 14:56:46 +0100

    update to etherscan api for price feed

--- pages/api/getEthUsd.ts
+++ pages/api/getEthUsd.ts
@@ -1,18 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import corsMiddleware from "@utils/corsMiddleware"
 import fetcher from "@utils/fetcher"
-import { ethers } from "ethers"
 
 const getQuotes = async (req: NextApiRequest, res: NextApiResponse) => {
-  // await corsMiddleware(req, res)
+  await corsMiddleware(req, res)
   try {
     if (req.method === "GET") {
-      const data = await fetcher(
-        "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
+      // TODO: Cache responses
+      const { result } = await fetcher(
+        `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API_KEY}`
       )
-      console.log(data)
 
-      res.status(200).json(data?.price)
+      res.status(200).json(result?.ethusd)
     }
   } catch (err) {
     console.log(err)
