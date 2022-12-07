5f6028fc36a6ab4e55c129bed6e9b22f27902a53
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-12-07 16:12:59 +0100

    use infura as main provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,8 +21,8 @@ const defaultChains =
   process.env.NEXT_PUBLIC_CHAIN_ID === "5" ? [chain.goerli] : [chain.mainnet]
 
 const { chains, provider } = configureChains(defaultChains, [
-  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
   infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_ID }),
+  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
   publicProvider()
 ])
 
