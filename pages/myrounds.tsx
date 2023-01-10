f7b38307d46e37ae4efb3e7fcd9df64074f2207c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-01-11 00:40:07 +0100

    updated og_image

--- pages/myrounds.tsx
+++ pages/myrounds.tsx
@@ -26,7 +26,7 @@ export default function Rounds() {
           url: domain,
           images: [
             {
-              url: `${domain}/og_image.jpg`,
+              url: `${domain}/og_image.png`,
               width: 1000,
               height: 1000,
               alt: `${defaultTitle} cover image`
--- pages/rounds/[id].tsx
+++ pages/rounds/[id].tsx
@@ -57,7 +57,7 @@ export default function Round({ subgraphData, projectData }) {
           url: domain,
           images: [
             {
-              url: `${domain}/og_image.jpg`,
+              url: `${domain}/og_image.png`,
               width: 1000,
               height: 1000,
               alt: `${defaultTitle} cover image`
