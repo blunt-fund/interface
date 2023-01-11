c5b115608223a64f9f5e439ae4b995b0d47e7fe9
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-01-11 22:01:16 +0100

    updated to next13 + fixes

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -43,7 +43,7 @@ const Button: FC<ButtonProps> = (props) => {
   const rootClassName = `nightwind-prevent px-7 h-[40px] min-w-[150px] focus:outline-none ${color} ${className}`
 
   return href ? (
-    <Link href={href} passHref>
+    <Link href={href} className="block">
       <button className={rootClassName}>{label}</button>
     </Link>
   ) : (
